aa0a659851d662489982c951e5ca5f0c6fb6cc06
LuncieWSLArch <luncie.vii@gmail.com>
2026-09-16 18:07:47 -0600

    feat: setup de entorno completo (zsh, p10k, lazyvim + dependencias core)

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # dotfiles
 Dotfiles config for zsh, p10k, neovim
+
+## Install
+
+```shell
+git clone git@github.com:luncied/dotfiles.git ~/dotfiles
+```
+```
+
+
+```shell
+cd ~/dotfiles
+chmod +x setup_dotfiles.sh
+./setup_dotfiles.sh
+```
+
+
+  On windows we need to move the dir of nvim to `~\AppData\Local\nvim`. We create a symlink to that directory.
+
+```powershell
+New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\nvim" -Target "$HOME\.config\nvim"
+```
+```
+```
--- p10k-user.zsh
+++ p10k-user.zsh
@@ -526,25 +526,25 @@ typeset -g POWERLEVEL9K_VCS_MODIFIED_FOREGROUND="#ef9f76"
 
   # Status on success. No content, just an icon. No need to show it if prompt_char is enabled as
   # it will signify success by turning green.
-  typeset -g POWERLEVEL9K_STATUS_OK=false
-typeset -g POWERLEVEL9K_STATUS_OK_FOREGROUND="#a6d189"
+  typeset -g POWERLEVEL9K_STATUS_OK=true
+  typeset -g POWERLEVEL9K_STATUS_OK_FOREGROUND="#a6e3a1"
   typeset -g POWERLEVEL9K_STATUS_OK_VISUAL_IDENTIFIER_EXPANSION='✔'
 
   # Status when some part of a pipe command fails but the overall exit status is zero. It may look
   # like this: 1|0.
   typeset -g POWERLEVEL9K_STATUS_OK_PIPE=true
-typeset -g POWERLEVEL9K_STATUS_OK_PIPE_FOREGROUND="#a6d189"
+  typeset -g POWERLEVEL9K_STATUS_OK_PIPE_FOREGROUND="#a6e3a1"
   typeset -g POWERLEVEL9K_STATUS_OK_PIPE_VISUAL_IDENTIFIER_EXPANSION='✔'
 
   # Status when it's just an error code (e.g., '1'). No need to show it if prompt_char is enabled as
   # it will signify error by turning red.
-  typeset -g POWERLEVEL9K_STATUS_ERROR=false
-typeset -g POWERLEVEL9K_STATUS_ERROR_FOREGROUND="#ea999c"
+  typeset -g POWERLEVEL9K_STATUS_ERROR=true
+  typeset -g POWERLEVEL9K_STATUS_ERROR_FOREGROUND="#eba0ac"
   typeset -g POWERLEVEL9K_STATUS_ERROR_VISUAL_IDENTIFIER_EXPANSION='✘'
 
   # Status when the last command was terminated by a signal.
   typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL=true
-typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_FOREGROUND="#ea999c"
+  typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_FOREGROUND="#eba0ac"
   # Use terse signal names: "INT" instead of "SIGINT(2)".
   typeset -g POWERLEVEL9K_STATUS_VERBOSE_SIGNAME=false
   typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_VISUAL_IDENTIFIER_EXPANSION='✘'
@@ -552,7 +552,7 @@ typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_FOREGROUND="#ea999c"
   # Status when some part of a pipe command fails and the overall exit status is also non-zero.
   # It may look like this: 1|0.
   typeset -g POWERLEVEL9K_STATUS_ERROR_PIPE=true
-typeset -g POWERLEVEL9K_STATUS_ERROR_PIPE_FOREGROUND="#ea999c"
+  typeset -g POWERLEVEL9K_STATUS_ERROR_PIPE_FOREGROUND="#eba0ac"
   typeset -g POWERLEVEL9K_STATUS_ERROR_PIPE_VISUAL_IDENTIFIER_EXPANSION='✘'
 
   ###################[ command_execution_time: duration of the last command ]###################
